624d6a4691dcd4b1afd219847f7e68197cdaf206
javid ibrahim <ibrupottayil@gmail.com>
2025-08-14 12:03:08 +0530

    report section

--- src/app/manager/project/[slug]/page.tsx
+++ src/app/manager/project/[slug]/page.tsx
@@ -1,7 +1,18 @@
 import ProjectDetail from "@/app/components/project/[slug]/page"
+import { useManangerAuthGuard } from "@/app/hooks/usemanagerAuthGuard"
 
 
 const ManagerProjectDetail = () => {
+    const { loading } = useManangerAuthGuard()
+
+    if (loading) return  (   
+        <div className="min-h-screen flex items-center justify-center">
+            <div className="text-center">
+            <div className="animate-spin rounded-full h-12 w-12 border-b-2 border-blue-600 mx-auto mb-4"></div>
+            <p className="text-gray-600">Loading...</p>
+            </div>
+        </div>
+    )
     return (
         <div>
             <ProjectDetail role="manager"/>
